121a62968e97e722e6068cf472e70615731af992
Stanley Sham <stanley.sham94@gmail.com>
2019-08-13 14:36:56 -0400

    completed assignment

---
+++ PokeGifs.py
@@ -0,0 +1,16 @@
+import json
+import requests
+import os
+
+# Pokemon API
+res = requests.get("http://pokeapi.co/api/v2/pokemon/pikachu/")
+body = json.loads(res.content)
+print(body["name"]) # should be "pikachu"
+print('')
+
+# Giphy API
+key = os.environ.get('GIPHY_KEY')
+url = (f"https://api.giphy.com/v1/gifs/search?api_key={key}&q=pikachu&rating=g")
+giphy_res = requests.get(url)
+body = json.loads(giphy_res.content)
+print(body['data'][0]['url'])
